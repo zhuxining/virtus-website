3b4f738a8ef6b36fe8e13c0872382a3eb9444d40
syneju <syneju@gmail.com>
2025-05-24 19:40:34 +0800

    feat:add prisma and better-auth

---
+++ prisma.config.ts
@@ -0,0 +1,31 @@
+import path from 'node:path';
+import { PrismaD1HTTP } from '@prisma/adapter-d1';
+import type { PrismaConfig } from 'prisma';
+
+// import your .env file
+import 'dotenv/config';
+
+type Env = {
+	DATABASE_URL: string;
+	CLOUDFLARE_D1_TOKEN: string;
+	CLOUDFLARE_ACCOUNT_ID: string;
+	CLOUDFLARE_DATABASE_ID: string;
+};
+
+export default {
+	earlyAccess: true,
+	schema: path.join('prisma'),
+
+	migrate: {
+		async adapter(env) {
+			// if (import.meta.env.NODE_ENV !== 'production') {
+			// 	return new PrismaBetterSQLite3({ url: env.DATABASE_URL });
+			// }
+			return new PrismaD1HTTP({
+				CLOUDFLARE_D1_TOKEN: env.CLOUDFLARE_D1_TOKEN,
+				CLOUDFLARE_ACCOUNT_ID: env.CLOUDFLARE_ACCOUNT_ID,
+				CLOUDFLARE_DATABASE_ID: env.CLOUDFLARE_DATABASE_ID,
+			});
+		},
+	},
+} satisfies PrismaConfig<Env>;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,30 @@
 import { faker } from '@faker-js/faker'
-
+import { PrismaD1HTTP } from '@prisma/adapter-d1'
 import { PrismaClient } from '~/generated/prisma'
+import 'dotenv/config'
 
-const prisma = new PrismaClient()
+const adapter = new PrismaD1HTTP({
+	CLOUDFLARE_D1_TOKEN: process.env.CLOUDFLARE_D1_TOKEN!,
+	CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID!,
+	CLOUDFLARE_DATABASE_ID: process.env.CLOUDFLARE_DATABASE_ID!,
+})
+
+const prisma = new PrismaClient({ adapter })
 
 async function seed() {
 	console.time('🌱 Database has been seeded')
 
-	const findUser = await prisma.user.findFirst()
+	const findPost = await prisma.post.findFirst()
 
-	if (findUser) {
+	if (findPost) {
 		return
 	}
 
-	await prisma.user.create({
+	await prisma.post.create({
 		data: {
+			id: faker.string.uuid(),
 			name: faker.person.fullName(),
-			email: faker.internet.email(),
+			description: faker.lorem.paragraph(),
 		},
 	})
 
